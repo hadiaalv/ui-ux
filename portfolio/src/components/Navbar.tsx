73652acfda35f7e9e8147b8d0771f709afd03d80
hadiaalvi2 <hadiaalvi18@gmail.com>
2026-08-01 01:22:50 +0500

    navbar

--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -23,7 +23,7 @@ export default function Navbar() {
     { name: "Home", href: "/" },
     { name: "Projects", href: "/projects" },
     { name: "About", href: "/about" },
-    { name: "Contact", href: "/contact" },
+    
   ];
 
   return (
