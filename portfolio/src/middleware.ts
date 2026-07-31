326c1db7858b1afd44cae3a33cda400b627144e2
hadiaalvi2 <hadiaalvi18@gmail.com>
2026-07-31 17:01:55 +0500

    password

--- portfolio/src/middleware.ts
+++ portfolio/src/middleware.ts
@@ -23,5 +23,7 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: "/:path*",
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
 };
